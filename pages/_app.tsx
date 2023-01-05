9c5596de8b0941b7bcacc7684f0e0d32a841eba9
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2023-01-05 20:47:49 +0100

    Fonts added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Noto_Sans } from '@next/font/google'
+
+const nanoSans = Noto_Sans({
+	weight: ['300', '400', '500', '700'],
+	style: 'normal',
+	subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main className={nanoSans.className}>
+			<Component {...pageProps} />
+		</main>
+	)
 }
